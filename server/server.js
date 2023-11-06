7da6823ffea959a1048b3ff6ed5204ed436cf37e
cruisershiva <shivakerur99@gmail.com>
2023-11-07 01:24:16 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -6,13 +6,17 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 const fs = require('fs');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+
 const { exec } = require('child_process');
 
 const app = express();
+
 app.use("/", express.static(__dirname + "/"));
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
@@ -23,12 +27,12 @@ const server = app.listen(port, () => {
 });
 
 mongoose.connect(`mongodb+srv://shivakerur99:shivanand99805257@cluster0.usva3cf.mongodb.net/`)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-  });
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
 
 const conversionSchema = new mongoose.Schema({
   filename: String,
@@ -58,7 +62,7 @@ app.post('/upload', upload.single('media'), async (req, res) => {
     const conversion = new Conversion({
       filename: req.file.originalname,
       text,
-      language: 'your_language_here', // Set the language as needed
+      language,
       date: new Date(),
     });
 
@@ -71,11 +75,13 @@ app.post('/upload', upload.single('media'), async (req, res) => {
     res.status(500).json({ error: 'An error occurred' });
   }
 });
+  
 
 app.get('/conversions/:id', async (req, res) => {
   try {
-    const conversion = await Conversion.findById(req.params.id);
-    res.json(conversion);
+    const conversions = await Conversion.findById(req.params.id);
+    // const conversions = await Conversion.find({}, '-_id -__v');
+    res.json(conversions);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'An error occurred' });
@@ -83,64 +89,203 @@ app.get('/conversions/:id', async (req, res) => {
 });
 
 async function convertMediaToText(filePath) {
+  // Check if the file is a video and extract audio
   if (filePath.endsWith('.mp4')) {
-    await convertVideoToAudio(filePath);
-    filePath = 'audio.wav';
-  } else if (filePath.endsWith('.mp3')) {
-    await convertAudioToWav(filePath);
-    filePath = 'audio2.wav';
+      await new Promise((resolve, reject) => {
+          ffmpeg(filePath)
+              .toFormat('wav')
+              .on('end', resolve)
+              .on('error', reject)
+              .save('D:/New folder/transcribe1/audio-vedio-transcriptor-main/server/audio.wav');
+      });
+      filePath = 'audio.wav';
+  }
+  else if (filePath.endsWith('.mp3')) {
+      await new Promise((resolve, reject) => {
+          ffmpeg(filePath)
+              .toFormat('wav')
+              .on('end', resolve)
+              .on('error', reject)
+              .save('D:/New folder/transcribe1/audio-vedio-transcriptor-main/server/audio2.wav');
+      });
+      filePath = 'audio2.wav';
   }
 
   try {
-    const text = await performTextRecognition(filePath);
-    return text;
+      const text = await convertToText(filePath);
+      return text;  // Return the result from convertToText
   } catch (error) {
-    console.error(error);
+      console.error(error);
+  }
+
+  async function convertToText(filePath) {
+      // Construct the Python command with the filePath variable
+      const command = `python main.py ${filePath}`; // Replace 'main.py' with your Python script's file name
+
+      return new Promise((resolve, reject) => {
+          exec(command, (error, stdout, stderr) => {
+              if (error) {
+                  console.error(`Error executing Python script: ${error}`);
+                  reject(error);
+                  return;
+              }
+
+              if (stderr) {
+                  console.error(`Python script encountered an error: ${stderr}`);
+                  reject(stderr);
+                  return;
+              }
+
+              const recognizedText = fs.readFileSync('recognized_text.txt', 'utf8');  // Clean up
+              fs.unlinkSync(filePath);
+              resolve(recognizedText);
+          });
+      });
   }
 }
+// const express = require('express');
+// const multer = require('multer');
+// const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+// const ffmpeg = require('fluent-ffmpeg');
+// ffmpeg.setFfmpegPath(ffmpegPath);
+// const fs = require('fs');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const { exec } = require('child_process');
 
-async function convertVideoToAudio(filePath) {
-  return new Promise((resolve, reject) => {
-    ffmpeg(filePath)
-      .toFormat('wav')
-      .on('end', resolve)
-      .on('error', reject)
-      .save('D:/New folder/transcribe1/audio-vedio-transcriptor-main/server/audio.wav');
-  });
-    filePath = 'audio.wav';
-}
+// const app = express();
+// app.use("/", express.static(__dirname + "/"));
+// app.use(express.json());
 
-async function convertAudioToWav(filePath) {
-  return new Promise((resolve, reject) => {
-    ffmpeg(filePath)
-      .toFormat('wav')
-      .on('end', resolve)
-      .on('error', reject)
-      .save('D:/New folder/transcribe1/audio-vedio-transcriptor-main/server/audio2.wav');
-  });
-    filePath = 'audio2.wav';
-}
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
-async function performTextRecognition(filePath) {
-  const command = `python main.py ${filePath}`; // Replace 'main.py' with your Python script's file name
+// const port = process.env.PORT || 9000;
 
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing Python script: ${error}`);
-        reject(error);
-        return;
-      }
+// const server = app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
 
-      if (stderr) {
-        console.error(`Python script encountered an error: ${stderr}`);
-        reject(stderr);
-        return;
-      }
+// mongoose.connect(`mongodb+srv://shivakerur99:shivanand99805257@cluster0.usva3cf.mongodb.net/`)
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((err) => {
+//     console.error('MongoDB connection error:', err);
+//   });
 
-      const recognizedText = fs.readFileSync('recognized_text.txt', 'utf8');
-      fs.unlinkSync(filePath);
-      resolve(recognizedText);
-    });
-  });
-}
+// const conversionSchema = new mongoose.Schema({
+//   filename: String,
+//   text: String,
+//   language: String,
+//   date: Date,
+// });
+
+// const Conversion = mongoose.model('Conversion', conversionSchema);
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads/');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage });
+// app.use(bodyParser.json());
+
+// app.post('/upload', upload.single('media'), async (req, res) => {
+//   try {
+//     const filePath = `uploads/${req.file.originalname}`;
+//     const text = await convertMediaToText(filePath);
+
+//     const conversion = new Conversion({
+//       filename: req.file.originalname,
+//       text,
+//       language: 'your_language_here', // Set the language as needed
+//       date: new Date(),
+//     });
+
+//     await conversion.save();
+//     console.log(conversion);
+
+//     res.json({ text });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: 'An error occurred' });
+//   }
+// });
+
+// app.get('/conversions/:id', async (req, res) => {
+//   try {
+//     const conversion = await Conversion.findById(req.params.id);
+//     res.json(conversion);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: 'An error occurred' });
+//   }
+// });
+
+// async function convertMediaToText(filePath) {
+//   if (filePath.endsWith('.mp4')) {
+//     await convertVideoToAudio(filePath);
+//     filePath = 'audio.wav';
+//   } else if (filePath.endsWith('.mp3')) {
+//     await convertAudioToWav(filePath);
+//     filePath = 'audio2.wav';
+//   }
+
+//   try {
+//     const text = await performTextRecognition(filePath);
+//     return text;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// async function convertVideoToAudio(filePath) {
+//   return new Promise((resolve, reject) => {
+//     ffmpeg(filePath)
+//       .toFormat('wav')
+//       .on('end', resolve)
+//       .on('error', reject)
+//       .save('D:/New folder/transcribe1/audio-vedio-transcriptor-main/server/audio.wav');
+//   });
+//     filePath = 'audio.wav';
+// }
+
+// async function convertAudioToWav(filePath) {
+//   return new Promise((resolve, reject) => {
+//     ffmpeg(filePath)
+//       .toFormat('wav')
+//       .on('end', resolve)
+//       .on('error', reject)
+//       .save('D:/New folder/transcribe1/audio-vedio-transcriptor-main/server/audio2.wav');
+//   });
+//     filePath = 'audio2.wav';
+// }
+
+// async function performTextRecognition(filePath) {
+//   const command = `python main.py ${filePath}`; // Replace 'main.py' with your Python script's file name
+
+//   return new Promise((resolve, reject) => {
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(`Error executing Python script: ${error}`);
+//         reject(error);
+//         return;
+//       }
+
+//       if (stderr) {
+//         console.error(`Python script encountered an error: ${stderr}`);
+//         reject(stderr);
+//         return;
+//       }
+
+//       const recognizedText = fs.readFileSync('recognized_text.txt', 'utf8');
+//       fs.unlinkSync(filePath);
+//       resolve(recognizedText);
+//     });
+//   });
+// }
