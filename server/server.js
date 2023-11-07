fa21c2de059075e0cc4b64fecc9568c034160319
cruisershiva <shivakerur99@gmail.com>
2023-11-08 03:34:25 +0530

    last

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-const port = 9000;
+const port = process.env.PORT || 9000;
 
 const server = app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
@@ -37,7 +37,7 @@ mongoose.connect(`mongodb+srv://shivakerur99:shivanand99805257@cluster0.usva3cf.
 const conversionSchema = new mongoose.Schema({
   filename: String,
   text: String,
-  date: Date,
+  timestamp: Date,
 });
 
 const Conversion = mongoose.model('Conversion', conversionSchema);
@@ -51,17 +51,18 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
 app.use(bodyParser.json());
 
 app.post('/upload', upload.single('media'), async (req, res) => {
   try {
     const filePath = `uploads/${req.file.originalname}`;
-    const text = await convertAudVidtoWav(filePath);
+    const text = await convertMediaToText(filePath);
 
     const conversion = new Conversion({
       filename: req.file.originalname,
       text,
-      date: new Date(),
+      timestamp: new Date(),
     });
 
     await conversion.save();
@@ -78,6 +79,7 @@ app.post('/upload', upload.single('media'), async (req, res) => {
 app.get('/conversions/:id', async (req, res) => {
   try {
     const conversions = await Conversion.findById(req.params.id);
+    // const conversions = await Conversion.find({}, '-_id -__v');
     res.json(conversions);
   } catch (err) {
     console.error(err);
@@ -85,7 +87,120 @@ app.get('/conversions/:id', async (req, res) => {
   }
 });
 
-async function convertAudVidtoWav(filePath) {
+
+
+// async function convertMediaToText(filePath) {
+//   // Check if the file is a video and extract audio
+//   if (filePath.endsWith('.mp4')) {
+//       await new Promise((resolve, reject) => {
+//           ffmpeg(filePath)
+//               .toFormat('wav')
+//               .on('end', resolve)
+//               .on('error', reject)
+//               .save('D:/New folder/transcribe/audio.wav');
+//       });
+//       filePath = 'audio.wav';
+//   }
+
+//   // Set the file path to your audio or video file
+//   try {
+//       const text = await convertToText(filePath);
+//       console.log(text);
+//   } catch (error) {
+//       console.error(error);
+//   }
+
+//   async function convertToText(filePath) {
+//       // Construct the Python command with the filePath variable
+//       const command = `python main.py ${filePath}`; // Replace 'main.py' with your Python script's file name
+
+//       return new Promise((resolve, reject) => {
+//           exec(command, (error, stdout, stderr) => {
+//               if (error) {
+//                   console.error(`Error executing Python script: ${error}`);
+//                   reject(error);
+//                   return;
+//               }
+
+//               if (stderr) {
+//                   console.error(`Python script encountered an error: ${stderr}`);
+//                   reject(stderr);
+//                   return;
+//               }
+
+//               resolve(stdout);
+//           });
+//       }).then((text) => {
+//           // Move the return statement here
+//           fs.unlinkSync(filePath);
+//           return text;
+//       });
+//   }
+// }
+
+
+// async function convertMediaToText(filePath) {
+//     // Check if the file is a video and extract audio
+//     if (filePath.endsWith('.mp4')) {
+//         await new Promise((resolve, reject) => {
+//             ffmpeg(filePath)
+//                 .toFormat('wav')
+//                 .on('end', resolve)
+//                 .on('error', reject)
+//                 .save('D:/New folder/transcribe1/audio-vedio-transcriptor-main/server/audio.wav');
+//         });
+//         filePath = 'audio.wav';
+//     }
+//     else if (filePath.endsWith('.mp3')) {
+//       await new Promise((resolve, reject) => {
+//           ffmpeg(filePath)
+//               .toFormat('wav')
+//               .on('end', resolve)
+//               .on('error', reject)
+//               .save('D:/New folder/transcribe1/audio-vedio-transcriptor-main/server/audio2.wav');
+//       });
+//       filePath = 'audio2.wav';
+//     }
+     
+//     // Set the file path to your audio or video file
+//     try {
+//         const text = await convertToText(filePath);
+//     } catch (error) {
+//         console.error(error);
+//     }
+
+//     async function convertToText(filePath) {
+//         // Construct the Python command with the filePath variable
+//         const command = `python main.py ${filePath}`; // Replace 'main.py' with your Python script's file name
+
+//         return new Promise((resolve, reject) => {
+//             exec(command, (error, stdout, stderr) => {
+//                 if (error) {
+//                     console.error(`Error executing Python script: ${error}`);
+//                     reject(error);
+//                     return;
+//                 }
+
+//                 if (stderr) {
+//                     console.error(`Python script encountered an error: ${stderr}`);
+//                     reject(stderr);
+//                     return;
+//                 }
+
+//                 const recognizedText = fs.readFileSync('recognized_text.txt', 'utf8');  // Clean up
+
+//                 resolve(recognizedText);
+//             });
+//         }).then((text) => {
+//           // Move the return statement here
+//           fs.unlinkSync(filePath);
+//           return text;
+//       })
+  
+//     }
+// }
+
+async function convertMediaToText(filePath) {
   // Check if the file is a video and extract audio
   if (filePath.endsWith('.mp4')) {
       await new Promise((resolve, reject) => {
@@ -109,14 +224,16 @@ async function convertAudVidtoWav(filePath) {
   }
 
   try {
-      const text = await performTextRecognition(filePath);
-      return text;  
+      const text = await convertToText(filePath);
+      return text;  // Return the result from convertToText
   } catch (error) {
       console.error(error);
   }
 
-  async function performTextRecognition(filePath) {
-      const command = `python main.py ${filePath}`; 
+  async function convertToText(filePath) {
+      // Construct the Python command with the filePath variable
+      const command = `python main.py ${filePath}`; // Replace 'main.py' with your Python script's file name
+
       return new Promise((resolve, reject) => {
           exec(command, (error, stdout, stderr) => {
               if (error) {
